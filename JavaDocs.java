8da8de37179c6e84451e401ae1bc9dc743c68985
Lawrence Liu <lawrence255@yahoo.com>
2016-01-07 12:22:51 -0500

    Basic pane for text created, not tested yet.

---
+++ JavaDocs.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class JavaDocs extends JFrame{ //implements ActionListener{
+    private Container pane;
+
+    
+    public JavaDocs(){
+	this.setTitle("JavaDocs");
+	this.setSize(600,400);
+	this.setLocation(100,100);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+	pane=this.getContentPane();
+    }
+
+}
